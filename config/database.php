6c5ad76209819eaa684d3c1fb9f86efe73e25ebc
GamaRL <gamarl1973@gmail.com>
2022-06-06 18:22:18 -0500

    ADD: database config

--- config/database.php
+++ config/database.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Str;
 
-$DATABASE_URL=parse_url('postgres://avxblxzaowtgev:8a5881738d73f47c6c0c6c0494ca12d7a2e9b20f13ac0146c7517999ceec0637@ec2-54-160-109-68.compute-1.amazonaws.com:5432/d1qiufk03jfrpf');
-
 return [
 
     /*
@@ -65,18 +63,19 @@ return [
             ]) : [],
         ],
 
-
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => $DATABASE_URL["host"],
-            'port' => $DATABASE_URL["port"],
-            'database' => ltrim($DATABASE_URL["path"], "/"),
-            'username' => $DATABASE_URL["user"],
-            'password' => $DATABASE_URL["pass"],
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'require',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
@@ -126,7 +125,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '') . '_database'),
         ],
 
         'default' => [
