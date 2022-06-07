8e685dcb03d527610049fd0e485ec925199a7d97
GamaRL <gamarl1973@gmail.com>
2022-06-06 19:08:37 -0500

    STYLE: Tabla

--- resources/views/components/schedules/calendar.blade.php
+++ resources/views/components/schedules/calendar.blade.php
@@ -2,7 +2,7 @@
 
 <div class="container">
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table class="w-full text-sm text-left text-gray-500">
+        <table class="w-full text-left text-gray-500">
             <thead class="text-xs text-gray-700 uppercase bg-indigo-100">
             <tr>
                 <th scope="col" class="text-center px-6 py-3">{{__('Hour')}}</th>
